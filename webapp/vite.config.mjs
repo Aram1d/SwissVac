be5a87b52511d7ba02c39bbaef53cda7d3600363
Wilfried <wsu287@gmail.com>
2025-08-13 14:30:53 +0200

    chore: updated dependencies

--- webapp/vite.config.mjs
+++ webapp/vite.config.mjs
@@ -1,44 +1,52 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 const manifestSource = {
-  registerType: 'autoUpdate',
-  includeAssests: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+  registerType: "autoUpdate",
+  includeAssests: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
   manifest: {
-    name: 'Swiss VAC',
-    short_name: 'swiss vac',
-    description: 'Swiss VAC atlas and VFR Manual',
+    name: "Swiss VAC",
+    short_name: "swiss vac",
+    description: "Swiss VAC atlas and VFR Manual",
     icons: [
-      { src: '/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
-      { src: '/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
     ],
-    theme_color: '#1971C2',
-    background_color: '#242424',
-    display: 'standalone',
-    scope: '/',
-    start_url: '/',
-    orientation: 'portrait',
+    theme_color: "#1971C2",
+    background_color: "#242424",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
   },
 };
 
 export default defineConfig({
   build: {
-    target: 'es2022',
+    target: "es2022",
   },
   esbuild: {
-    target: 'es2022',
+    target: "es2022",
   },
   optimizeDeps: {
     esbuildOptions: {
-      target: 'es2022',
+      target: "es2022",
     },
   },
   plugins: [react(), tsconfigPaths(), VitePWA(manifestSource)],
   resolve: {
     alias: {
-      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+      "@tabler/icons-react": "@tabler/icons-react/dist/esm/icons/index.mjs",
     },
   },
   define: {
@@ -46,7 +54,7 @@ export default defineConfig({
   },
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './vitest.setup.mjs',
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.mjs",
   },
 });
